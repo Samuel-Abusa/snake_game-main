d4fa73e1373360238ef24080ef2ec28ded28fd41
samuel <samuelabusakennedy@gmail.com>
2024-07-21 16:00:04 +0300

    made collision with the tail possible and had the score board class handle everything related to the score

--- game.py
+++ game.py
@@ -4,19 +4,20 @@ from turtle import Turtle
 from random import randint
 from scoreboard import Scoreboard
 
+COLLISSION_DISTANCE = 15
+
 
 class Game:
     def __init__(self) -> None:
         self.segments = []
-        self.create_snake(7)
+        self.add_segment(3)
         self.head = self.segments[0]
-        self.score = 0
         self.food = Turtle("circle")
         self.food.color("orange")
         self.food.penup()
         self.food.speed("fastest")
 
-    def create_snake(self, segment_length):
+    def add_segment(self, segment_length):
         for n in range(segment_length):
             segment = Turtle("square")
             segment.color("white")
@@ -25,7 +26,7 @@ class Game:
             if segment_length > 1:
                 segment.goto(x=-n * 20, y=0)
             else:
-                segment.goto(self.segments[len(self.segments) - 1].position())
+                segment.goto(self.segments[-1].position())
             self.segments.append(segment)
             segment.showturtle()
 
@@ -35,21 +36,27 @@ class Game:
         self.food.goto(random_xcor, random_ycor)
 
     def game_over(self, screen_height, screen_width, board):
+        tail_collision = False
+
+        for segment in self.segments[1:]:
+            if segment.distance(self.head) <= COLLISSION_DISTANCE:
+                tail_collision = True
+
         if (
             self.head.xcor() > (screen_width / 2)
             or self.head.xcor() < -(screen_width / 2)
             or self.head.ycor() > (screen_height / 2)
             or self.head.ycor() < -(screen_height / 2)
+            or tail_collision
         ):
-            board.final_score(self.score)
+            board.final_score()
             turtle.done()
+            return True
 
     def play(self, screen):
         s_width = screen.window_width()
         s_height = screen.window_height()
         scoreboard = Scoreboard(s_height)
-
-        scoreboard.update_score(self.score)
         self.refresh_food(s_width, s_height)
 
         while True:
@@ -61,12 +68,12 @@ class Game:
                 new_ycor = self.segments[segment_num - 1].ycor()
                 self.segments[segment_num].goto(new_xcor, new_ycor)
 
-            self.head.forward(10)
+            self.head.forward(20)
 
-            self.game_over(s_height, s_width, scoreboard)
+            if self.game_over(s_height, s_width, scoreboard):
+                break
 
-            if self.food.distance(self.head) <= 15:
-                self.score += 1
-                self.create_snake(1)
-                scoreboard.update_score(self.score)
+            if self.food.distance(self.head) <= COLLISSION_DISTANCE:
+                self.add_segment(1)
+                scoreboard.update_score()
                 self.refresh_food(s_width, s_height)
